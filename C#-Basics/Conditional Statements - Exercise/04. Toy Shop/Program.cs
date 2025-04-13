4c54edfe9625a3303d1a8c7e03c4c8e8da43b25c
Myumyun1000 <myumyunmurad1000@gmail.com>
2025-04-13 16:38:26 +0300

    Added C#-Basics

---
+++ C#-Basics/Conditional Statements - Exercise/04. Toy Shop/Program.cs
@@ -0,0 +1,25 @@
+﻿double tripPrice = double.Parse(Console.ReadLine());
+int puzzles = int.Parse(Console.ReadLine());
+int talkingDolls = int.Parse(Console.ReadLine());
+int teddies = int.Parse(Console.ReadLine());
+int minions = int.Parse(Console.ReadLine());
+int trucks = int.Parse(Console.ReadLine());
+
+double totalPrice = puzzles * 2.60 + talkingDolls * 3.00 + teddies * 4.10 + minions * 8.20 + trucks * 2.00;
+
+if (puzzles + talkingDolls + teddies + minions + trucks >= 50)
+{
+    totalPrice *= 0.75;
+}
+
+totalPrice *= 0.90;
+
+if (totalPrice >= tripPrice)
+{
+    Console.WriteLine($"Yes! {totalPrice - tripPrice:F2} lv left.");
+}
+else
+{
+    Console.WriteLine($"Not enough money! {tripPrice - totalPrice:F2} lv needed.");
+}
+
---
+++ C#-Basics/First Steps In Coding - Exercise/09. Fish Tank/Program.cs
@@ -0,0 +1,9 @@
+﻿int duljinasm = int.Parse(Console.ReadLine());
+int shirochinasm = int.Parse(Console.ReadLine());
+int visochina = int.Parse(Console.ReadLine());
+double procent = double.Parse(Console.ReadLine());
+int obemnnaakvarium = duljinasm * shirochinasm * visochina;
+double obemvlitri = obemnnaakvarium * 0.001;
+double zaetoprostranstvo = procent / 100;
+double nujnilitri = obemvlitri * (1 - zaetoprostranstvo);
+Console.WriteLine(nujnilitri);
